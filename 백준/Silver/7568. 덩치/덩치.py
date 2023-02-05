677bf471c866b36d396076fabaedf45ab19b1c06
MignonY <122513665+MignonY@users.noreply.github.com>
2023-02-06 08:07:46 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -9,8 +9,6 @@ stu = [list((map(int, sys.stdin.readline().split()))) for _ in range(N)]
 for i in range(len(stu)):
     cnt = 1
     for j in range(len(stu)):
-        if stu[i][0] != stu[j][0] and stu[i][1] != stu[j][1]:
-            if stu[i][0] < stu[j][0] and stu[i][1] < stu[j][1]:
-                cnt += 1
-
-    print(cnt, end=' ')
+        if stu[i][0] < stu[j][0] and stu[i][1] < stu[j][1]:
+            cnt += 1
+    print(cnt)
