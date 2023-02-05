19714b68285a9a6c327e4ac731f59891fe232197
MignonY <122513665+MignonY@users.noreply.github.com>
2023-02-05 20:31:03 +0900

    [Bronze II] Title: 분해합, Time: 2668 ms, Memory: 206476 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,26 @@
+import sys
+N = int(sys.stdin.readline())
+
+numls = [i for i in range(1, 1000001)]
+
+def BHH(n):
+    ls = list(str(n)) + [n]
+    ls = list(map(int, ls))
+    return ls
+
+finls = list(map(BHH, numls))
+ssjls = []
+for i in range(len(finls)):
+    if sum(finls[i]) == N:
+        ssjls.append(finls[i][-1])
+
+#
+# min = 100000001
+# for i in range(len(ssjls)):
+#     if min > ssjls[i]:
+#         min = ssjls[i]
+
+if len(ssjls) == 0:
+    print(0)
+else:
+    print(min(ssjls))
