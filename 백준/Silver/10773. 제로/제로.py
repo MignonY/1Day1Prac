118832aeb6929bcc6c2b3270be7afef07123940b
MignonY <122513665+MignonY@users.noreply.github.com>
2023-02-13 23:32:09 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+K = int(input())
+stack = []
+for i in range(K):
+    num = int(input())
+    if num != 0:
+        stack.append(num)
+    else:
+        stack.pop()
+# print(stack)
+print(sum(stack))
