8ab553382c7dc47df8f0285969a3f08da260c6c9
MignonY <122513665+MignonY@users.noreply.github.com>
2023-02-23 09:59:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 304 ms, Memory: 50244 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,33 +1,32 @@
-N = int(input())
-visit = [0 for _ in range(N+1)]
-graph = [[] for _ in range(N+1)]
-ans = [0 for _ in range(N+1)]
-queue = [0 for _ in range(101000)]
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+
+N=int(input())
+visited=[False]*(N+1)
+answer=[0]*(N+1)
+E=[[] for _ in range(N+1)]
 for i in range(N-1):
-    u, v = map(int,input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    S,D=map(int,input().split())
+    E[S].append(D)
+    E[D].append(S)
 
-# print(graph)
 
-def BFS(start_v):
-    rear = -1
-    front = 0
-    rear +=1
-    queue[rear] = start_v
-    visit[start_v] = 1
-    while rear +1 != front:
-        current =queue[front]
-        front +=1
-        for i in graph[current]:
-            if visit[i] ==0:
-                # print(i)
-                rear += 1
-                queue[rear] = i
-                visit[i] = 1
-                ans[i] = current
+def bfs(E,v,visited):
+    que=deque([v])
+    visited[v]=True
+    while que:
+        x=que.popleft()
+        for i in E[x]:
+            if not visited[i]:
+                answer[i]=x
+                que.append(i)
+                visited[i]=True
+           
 
-BFS(1)
-for i in range(2,len(ans)):
-    print(ans[i])
+            
+bfs(E,1,visited)
 
+for i in range(2,N+1):
+        print(answer[i])
