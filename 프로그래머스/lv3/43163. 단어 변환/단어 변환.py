cb7a9db042c93b3ce3ba04ab91f87c757240d7b4
MignonY <122513665+MignonY@users.noreply.github.com>
2023-03-27 15:43:54 +0900

    [level 3] Title: 단어 변환, Time: 0.26 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,50 +1,51 @@
+
 def solution(begin, target, words):
-	global start, final, wordls, answer, visit, savest
-	start = begin
-	savest = begin
-	final = target
-	wordls = words
-	visit = [0 for _ in range(len(wordls))]
-	answer = 100000
-	if final not in wordls:
-		answer = 0
-	else:
-		DFS(0,0)
-	print(answer)
-	return answer
+    global begin_,target_,words_,visit,queue
+    begin_= begin
+    target_= target
+    words_= words
+    visit = [0 for _ in range(len(words_))]
+    queue = [0 for _ in range(10000)]
 
-temp = []
-def DFS(depth,bg):
-	global start, savest, answer
-	# print(temp, depth,start)
-	if depth == len(wordls):
-		# print(temp,'final',depth)
-		return
+    answer = BFS(begin,0)
+    print(answer)
+    # answer = 0
+    return answer
 
-	for i in range(len(wordls)):
-		if visit[i] == 0:
-			# if temp and i < temp[-1]:
-			# 	continue
-			cnt = 0
-			for j in range(len(start)):
-				if start[j] == wordls[i][j]:
-					cnt += 1
-			if cnt == len(start)-1:
-				if wordls[i] == final:
-					print(temp)
-					if answer > depth+1:
-						answer = depth+1
-						continue
-				temp.append(i)
-				start = wordls[i]
-				visit[i] = 1
-				DFS(depth+1, i+1)
-				if temp:
-					visit[temp[-1]] = 0
-					temp.pop()
-				if temp:
-					start = wordls[temp[-1]]
-				else:
-					start = savest
 
-# solution('hit','cog',["hot","dot","dog","lot","log","cog"])
+def BFS(word, depth):
+    global visit
+    rear = -1
+    front = 0
+    rear += 1
+    depth = 0
+    queue[rear] = (word, depth, visit)
+
+    while rear +1 != front:
+        current = queue[front][0]
+        depth = queue[front][1]
+        visit = list(queue[front][2])
+        front += 1
+        # print(f'current:{current}')
+        if current == target_:
+            return depth
+
+        for i in range(len(words_)):
+            cnt = 0
+            # print(i,words_[i])
+            for j in range(len(words_[i])):
+                if visit[i] != 1 and (words_[i][j] == current[j]):
+                    cnt += 1
+            if cnt == len(words_[i])-1:
+                # print(words_[i])
+                rear += 1
+                visit[i] = 1
+                # print(visit,'visit')
+                queue[rear] = (words_[i], depth+1, list(visit))
+                visit[i] = 0
+
+
+        if rear +1 == front:
+            if current != target_:
+                return 0
+
