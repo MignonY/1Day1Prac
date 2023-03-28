14bbd4ecca233eadd415da19d56c424672f6ca50
MignonY <122513665+MignonY@users.noreply.github.com>
2023-03-28 23:21:10 +0900

    [Bronze IV] Title: 탁구 경기, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	int a; char win; int Dnum = 0; int Pnum = 0;
+	scanf("%d", &a);
+	bool Flag = true;
+	for(int i=0; i<a; i++){
+		scanf(" %c", &win);
+		if(win == 'D'){
+			Dnum += 1;
+		}
+		else if(win == 'P'){
+			Pnum += 1;
+		}
+//		printf("%d,%d,%d\n",i,Dnum,Pnum);
+		if(Pnum-Dnum == 2 || Dnum-Pnum == 2){
+			printf("%d:%d\n",Dnum,Pnum);
+			Flag = false;
+			break;
+		}
+	}	
+	if (Flag){	
+	printf("%d:%d\n", Dnum,Pnum);
+	}
+	return 0;
+}
