cec7f9992affe050cb7850170735ab64a6085196
MignonY <122513665+MignonY@users.noreply.github.com>
2023-03-30 00:37:06 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+	int a; int b; int c; int d; int e; int f;
+
+	scanf("%d" "%d" "%d" "%d" "%d" "%d", &a,&b,&c,&d,&e,&f);
+	for (int i =-999; i<=999; ++i){
+		for (int j=-999; j<=999; ++j){
+			if (a*i + b*j == c && d*i + e*j == f){
+				printf("%d %d",i,j);
+			}
+		}
+	}
+	
+	
+	return 0;
+}
