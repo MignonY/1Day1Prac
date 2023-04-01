44c532dfe163cffe110d2bc87c4b7d4c2eed0abb
MignonY <122513665+MignonY@users.noreply.github.com>
2023-04-01 23:54:15 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char word[105];
+int size;
+
+int main(){
+	
+	
+	scanf("%s", word);
+
+	for (int i=0; i<105; ++i){
+		if (word[i]==NULL)
+			break;
+		size++;	
+
+	}
+	printf("%d", size);
+		
+	return 0;
+}
