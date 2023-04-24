ae7281f05b6eb173f054ae8a80cb6c4c737b3975
MignonY <122513665+MignonY@users.noreply.github.com>
2023-04-24 22:55:45 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 using namespace std;
 
-unsigned long long starti, midi, endi, start2, mid2, end2, answer, minv;
+long long starti, midi, endi, start2, mid2, end2, answer, minv;
 unsigned long long temp;
 int N;
 bool Flag;
 
-unsigned long long solution(int n, vector<int> times) {
-    unsigned long long answer = 0;
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
 
     N = n;
     int tsize = times.size();
@@ -19,20 +19,22 @@ unsigned long long solution(int n, vector<int> times) {
 //		printf("%lld\n" ,temp);
 		midi = (starti+ endi)/2;
 //		printf("start:%lld , mid:%lld, end:%lld\n", starti, midi, endi);
-		if (endi - starti <= 1)
-			break;
+		if (endi - starti <= 1){
+				answer =  endi;
+				break;
+		}
 			
 		
 		temp = 0;
 		Flag = true;
 	    for(int i=0; i<tsize ; ++i){
     		temp +=(midi / times[i]);
-    		if (temp > N){
+    		if (temp >= N){
     			Flag = false;
     			break;
 			}
 		}
-		
+		 
 //		printf("start:%lld , mid:%lld, end:%lld, flag:%d, N:%d, temp:%lld\n", starti, midi, endi, Flag, N, temp);
 		
 		if(!Flag){
@@ -43,55 +45,14 @@ unsigned long long solution(int n, vector<int> times) {
 //			printf("case 2\n");
 			starti = midi;
 		}
-		else if (Flag && temp == N){
-//			printf("case 3\n");
-			answer = midi;
-			break;
-		}
 	}
-    // printf("%lld\n", answer);
+
     
-    
-    end2 = answer;
-    start2 = 0;
-    minv = answer;
-//    temp = 0;
-    while(1){
-		
-    	unsigned long long mid2 = (start2+ end2)/2;
-    	
-    	if (end2-start2<=1)
-    		break;
-    	
-    	temp = 0;
-//    	printf("start2:%lld , mid2:%lld, end2:%lld, flag:%d, N:%d, temp:%lld\n", start2, mid2, end2, Flag, N, temp);
-		Flag = true;
-	    for(int i=0; i<tsize ; ++i){
-    		temp +=(mid2 / times[i]);
-    		if (temp > N){
-    			Flag = false;
-    			break;
-			}
-		}
-//		printf("Astart2:%lld , mid2:%lld, end2:%lld, flag:%d, N:%d, temp:%lld\n", start2, mid2, end2, Flag, N, temp);
-		
-		if(!Flag){
-//			printf("case 1\n");
-			end2 = mid2;
-		}
-		else if (Flag && temp < N){
-//			printf("case 2\n");
-			start2 = mid2;
-		}
-		else if (Flag && temp == N){
-			if (mid2 < minv)
-				minv = mid2;
-			end2 = mid2;
-//			printf("case 3, mid2=%lld, minv=%lld\n", mid2, minv);
-		}
-	}
-	answer = minv;
-	// printf("%lld\n", answer);
+//    end2 = answer;
+//    start2 = 0;
+//    minv = answer;
+//	answer = endi;
+	printf("%lld\n", answer);
     
     return answer;
 }
