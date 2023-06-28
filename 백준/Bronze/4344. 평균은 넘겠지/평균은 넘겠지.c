20137f00caf5f2eec44d6c9a8af8a83473c85ae7
MignonY <122513665+MignonY@users.noreply.github.com>
2023-06-28 21:04:53 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int num, N;
+
+int main() {
+    scanf("%d", &num);
+    
+    while(num--) {
+        scanf("%d", &N);
+        int score[N], sum=0, high=0;
+        double avg = 0;
+        for(int i=0; i<N; i++) {
+            scanf("%d", score+i);
+            sum += score[i];
+        }           
+        avg = (double)sum/N;
+        for(int i=0; i<N; i++) if(score[i] > avg) high++;
+        printf("%.3lf%%\n", (double)high*100/N);
+    }
+    return 0;
+}
