e6b9c7955b16f50f074885a9b20d583121612158
MignonY <122513665+MignonY@users.noreply.github.com>
2023-07-25 23:07:06 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int T, N, L[100];
+char S[100][21];
+
+int main() {
+	scanf("%d", &T);			
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d", &N);
+		int max_num = 0;			
+		int max_sul = 0;				
+		
+		for (int j = 0; j < N; j++) {
+			scanf("%s%d", &S[j], &L[j]);
+			if (L[j] > max_sul) {		
+				max_sul = L[j];
+				max_num = j;
+			}
+		}
+		printf("%s\n", S[max_num]);
+	}
+
+}
