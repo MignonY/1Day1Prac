8dfedd45fcdc1a05e40c2ad0aecdaf0413250eb2
MignonY <122513665+MignonY@users.noreply.github.com>
2023-07-28 22:29:33 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int k, a, d;
+
+int main() {
+    scanf("%d/%d/%d", &k, &d, &a);
+ 
+    if (k + a < d || d==0) {
+        printf("hasu");
+    }
+    else printf("gosu");
+}
+ 
