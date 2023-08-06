efd2e5404a7571214d69c381b607a8af740a053e
MignonY <122513665+MignonY@users.noreply.github.com>
2023-08-06 23:15:16 +0900

    [Bronze II] Title: NN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11944. NN/NN.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char n[5];
+int m;
+int main(){
+    scanf("%s %d", n, &m);
+    int sz = strlen(n);
+    int val = atoi(n);
+    int k = min(sz*val, m);
+    for(int i = 0; i < k; i++){
+        printf("%c", n[i%sz]);
+    }
+}
