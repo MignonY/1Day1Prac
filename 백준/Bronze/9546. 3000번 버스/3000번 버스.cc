8a1f6a26dcd26b8be9fcb94b7659d06f140b482f
MignonY <122513665+MignonY@users.noreply.github.com>
2023-09-14 22:36:21 +0900

    [Bronze II] Title: 3000번 버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9546. 3000번 버스/3000번 버스.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n; 
+
+int main(){
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int tmp;
+        cin >> tmp;
+        int cnt = 1;
+        int pre = 1;
+        for(int j = 1; j < tmp; j++){
+            cnt += (pre * 2);
+            pre = pre * 2;
+        }
+        cout << cnt << '\n';
+    }
+    return 0;
+}
