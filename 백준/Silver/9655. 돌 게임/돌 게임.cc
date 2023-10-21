c376d6dbb24069ce07d4ac19680d58e556a32f51
MignonY <122513665+MignonY@users.noreply.github.com>
2023-10-21 23:53:54 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int n;
+int main(){    
+    scanf("%d", &n);
+    if (n % 2 == 1){
+        printf("SK\n");
+    }   
+    else{
+        printf("CY\n");
+    }
+    return 0;
+}
