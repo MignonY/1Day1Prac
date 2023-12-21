ae8c7ac62e7ff8a438c40c2d9d433aaec71047d0
MignonY <122513665+MignonY@users.noreply.github.com>
2023-12-21 23:24:28 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 92 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ballPos, k, ans;
+vector <int> cups(200005);
+
+int main(){
+  cin >> n >> ballPos >> k;
+
+  for(int i = 1; i <= n; i++){
+    cups[i] = i;
+  }
+
+  for(int i = 0, a, b; i < k; i++){
+    cin >> a >> b;
+    swap(cups[a], cups[b]);
+  }
+  for(int i = 1; i <= n; i++){
+    if(cups[i] == ballPos) {
+      ans = i;
+      break;
+    }
+  }
+
+  cout << ans;
+}
