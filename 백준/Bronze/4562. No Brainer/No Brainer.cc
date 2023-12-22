06fa64d4f1a3b4cb0b20ca300eb66699299902f0
MignonY <122513665+MignonY@users.noreply.github.com>
2023-12-23 00:00:02 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		if (a < b) { cout << "NO BRAINS" << endl; }
+		else { cout << "MMM BRAINS" << endl; }
+	}
+}
